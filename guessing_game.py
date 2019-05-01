8104568a7b8a4db054153ed4813c74c6dbf59bf8
hurlio33 <jdhurley@gmail.com>
2019-05-01 16:21:26 -0700

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -2,11 +2,8 @@
 Python Web Development Techdegree
 Project 1 - Number Guessing Game
 --------------------------------
-
 For this first project we will be using Workspaces. 
-
 NOTE: If you strongly prefer to work locally on your own computer, you can totally do that by clicking: File -> Download Workspace in the file menu after you fork the snapshot of this workspace.
-
 """
 
 import random
@@ -17,38 +14,35 @@ attempts = (0)
 def start_game():        
     print("Welcome to the game. Guess a number between 1-10.")
     answer = random.randint(1, 10)
-    guess = input("Guess the number: ")
-    guess = int(guess)  
-    while guess:
+    guess = int(input("Guess a number: "))   
+
+    while guess:      
         global attempts
         attempts += 1
+
         try:
             if guess > answer:
                 print("It's lower")            
-                guess = input("Guess the number: ")
-                guess = int(guess)
+                guess = int(input("Guess a new number: "))
+                continue
             elif guess < answer:
                 print("It's higher")
-                guess = input("Guess the number: ")
-                guess = int(guess)       
+                guess = int(input("Guess a new number: "))
+                continue
             else: 
                 print("Got it")
-                print("It took you {} attempts".format(attempts))
+                print("It took you {} attempts".format(attempts))  
                 break
+    
         except ValueError:
-            print("Guess must be a number between 1-10")
-            guess = input("Guess the number: ")
-            guess = int(guess)             
+                print("Guess must be a number between 1-10")
+                           
         except TypeError:
-            print("Guess must be a number between 1-10")
-            guess = input("Guess the number: ")
-            guess = int(guess) 
-            
-                
-                                   
-                
+                print("Guess must be a number between 1-10")
                 
 
+                
+                        
     """Psuedo-code Hints
     
     When the program starts, we want to:
@@ -72,3 +66,4 @@ if __name__ == '__main__':
     # Kick off the program by calling the start_game function.
     start_game()
     
+
